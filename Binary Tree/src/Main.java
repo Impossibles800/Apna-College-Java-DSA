c0d9cd434891df2417858d10da7130542be9d773
Abhiraj <195514@nith.ac.in>
2022-11-07 08:47:26 +0530

    07/11/2022

--- Binary Tree/src/Main.java
+++ Binary Tree/src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Build_Tree.Node root = new Build_Tree.Node(1);
+        root.right = new Build_Tree.Node(3);
+        root.left.left = new Build_Tree.Node(4);
+        root.left.right = new Build_Tree.Node(5);
+        root.right.left = new Build_Tree.Node(6);
+        root.right.right = new Build_Tree.Node(7);
+//        System.out.println(Height_of_the_tree.height(root));
+//        System.out.println(Count_Nodes.count_nodes(root));
+        System.out.println(Sum_of_nodes.sum(root));
     }
 }
