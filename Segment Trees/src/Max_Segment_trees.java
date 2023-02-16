2f55b53d55bf0e67752a4b62593cf418d7ed5539
Abhiraj <195514@nith.ac.in>
2023-02-16 14:46:23 +0530

    16/02/2023

---
+++ Segment Trees/src/Max_Segment_trees.java
@@ -0,0 +1,45 @@
+public class Max_Segment_trees {
+    static int[] tree;
+
+    public static void init(int n) {
+        tree = new int[4 * n];
+    }
+
+    public static void buildSegmentTree(int i, int si, int sj, int[] a) {
+        if (si == sj) {
+            tree[i] = a[si];
+            return;
+        }
+        int mid = si + (sj - si) / 2;
+        buildSegmentTree(2 * i + 1, si, mid, a);
+        buildSegmentTree(2 * i + 2, mid + 1, sj, a);
+        tree[i] = Math.max(tree[2 * i + 1], tree[2 * i + 2]);
+    }
+
+    public static int getMax(int a[], int qi, int qj) {
+        int n = a.length;
+        return getMaxUtil(0, 0, n - 1, qi, qj);
+    }
+
+    public static int getMaxUtil(int i, int si, int sj, int qi, int qj) {
+
+
+        if (si > qj || sj < qi)
+            return Integer.MIN_VALUE;
+        else if (qi <= si && qj >= sj)
+            return tree[i];
+        int mid = si + (sj - si) / 2;
+        return Math.max(getMaxUtil(i, si, mid, qi, qj), getMaxUtil(i, mid + 1, sj, qi, qj));
+
+    }
+
+    public static void main(String[] args) {
+        int a[] = {6, 8, -1, 2, 17, 1, 3, 2, 4};
+        init(a.length);
+        buildSegmentTree(0, 0, a.length - 1, a);
+//        for (int i : tree) {
+//            System.out.print(i + " ");
+//        }
+        System.out.println(getMax(a, 2, 5));
+    }
+}
