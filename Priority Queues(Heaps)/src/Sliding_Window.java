5dc0c70782c1d7f9bfe4024cf44c475f290709fc
Abhiraj <195514@nith.ac.in>
2023-04-29 15:33:14 +0530

    13/11/2022

--- Priority Queues(Heaps)/src/Sliding_Window.java
+++ Priority Queues(Heaps)/src/Sliding_Window.java
@@ -1,38 +1,41 @@
 import java.util.PriorityQueue;
 
 public class Sliding_Window {
-    static class Pair implements Comparable<Pair>{
+    static class Pair implements Comparable<Pair> {
+
         int val;
         int idx;
-        Pair(int val, int idx){
-            this.idx=idx;
-            this.val=val;
+
+        Pair(int val, int idx) {
+            this.val = val;
+            this.idx = idx;
         }
+
         @Override
-        public int compareTo(Pair p){
-            return p.idx - this.idx;
+        public int compareTo(Pair p2) {
+            return p2.val - this.val;
         }
     }
+
     public static void main(String[] args) {
-        int []arr ={1,3,-1,-3,5,3,6,7};
+        int a[] = {1, 3, -1, -3, 5, 3, 6, 7};
         int k = 3;
-        int []res = new int[arr.length-k+1];
+        int res[] = new int[a.length - k + 1];
         PriorityQueue<Pair> pq = new PriorityQueue<>();
 
-//        Storing First window elements
         for (int i = 0; i < k; i++) {
-            pq.add(new Pair(arr[i],i));
+            pq.add(new Pair(a[i], i));
         }
         res[0] = pq.peek().val;
 
-        for (int i = k; i < arr.length ; i++) {
-            while(pq.size()>0 && pq.peek().idx<=(i-k))
+        for (int i = k; i < a.length; i++) {
+            while (pq.size() > 0 && pq.peek().idx <= (i - k))
                 pq.remove();
-            pq.add(new Pair(arr[i],i ));
-            res[i-k+1] = pq.peek().val;
+            pq.add(new Pair(a[i], i));
+            res[i - k + 1] = pq.peek().val;
         }
-        for (int i = 0; i < res.length; i++) {
-            System.out.println(res[i]);
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
         }
     }
 }
