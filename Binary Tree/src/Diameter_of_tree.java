a8a68824af5778a61f293092466cf62733af631c
Abhiraj <195514@nith.ac.in>
2023-05-23 11:47:46 +0530

    Done

--- Binary Tree/src/Diameter_of_tree.java
+++ Binary Tree/src/Diameter_of_tree.java
@@ -1,23 +1,23 @@
 public class Diameter_of_tree extends Build_Tree {
-    public static int height(Node root){
-
-        if(root == null){
-            return 0;
-        }
-        int left = height(root.left);
-        int right = height(root.right);
-        return Math.max(left, right) + 1;
-    }
-    public static int diameter(Node root){
-        if(root==null)
-            return 0;
-        int lh = height(root.left);
-        int ld = diameter(root.left);
-        int rh = height(root.right);
-        int rd = diameter(root.right);
-        int self_diameter = lh+rh+1;
-        return Math.max(Math.max(ld, rd), self_diameter);
-    }
+//    public static int height(Node root){
+//
+//        if(root == null){
+//            return 0;
+//        }
+//        int left = height(root.left);
+//        int right = height(root.right);
+//        return Math.max(left, right) + 1;
+//    }
+//    public static int diameter(Node root){
+//        if(root==null)
+//            return 0;
+//        int lh = height(root.left);
+//        int ld = diameter(root.left);
+//        int rh = height(root.right);
+//        int rd = diameter(root.right);
+//        int self_diameter = lh+rh+1;
+//        return Math.max(Math.max(ld, rd), self_diameter);
+//    }
     static class Info{
         int height;
         int diameter;
@@ -36,5 +36,8 @@ public class Diameter_of_tree extends Build_Tree {
         int height = Math.max(left_info.height, right_info.height);
 
         return new Info(diameter, height);
+    }
+    public static void main(String[] args) {
+        Node root = Tree_Traversal.buildTree();
     }
 }
